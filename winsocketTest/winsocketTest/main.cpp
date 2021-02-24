d5a1c2958a584570c037ef884cc5b22485fdf520
qs991011 <790165729@qq.com>
2021-02-24 17:44:59 +0800

    Add files via upload

--- winsocketTest/winsocketTest/main.cpp
+++ winsocketTest/winsocketTest/main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <WinSock2.h>
-#include "yyjson.h"
+#include <string.h>
+
 #pragma comment(lib,"ws2_32.lib")
 #pragma warning(disable:4996)
-#define PORT 9999   
+#define PORT 80   
 #define BUFFLEN 1024
 #define MAXLEN 8192 /* Max text line length */
 #define MAXBUF 8192 /* Max I/O buffer size */
 
-
-
+using namespace std;
+void doit(SOCKET fd);
+int parse_uri(std::string  uri, std::string& filename, std::string& cgiargs);
+void serve_dynamic(int fd, char *filename, char *cgiargs);
 int main(int argc, char **argv)
 {
-	
-
 	WSADATA wsaData;
 	//1 启动并初始化winsock
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData))
@@ -43,42 +44,115 @@ int main(int argc, char **argv)
 		closesocket(sServer);
 		WSACleanup();
 	}
-	//6 等待客户端连接
-	sockaddr_in addrClient;
-	int addrClientLen = sizeof(addrClient);
-	std::cout << "服务器启动成功,等待客户端连接中。。。" << std::endl;
-	SOCKET sClient = accept(sServer, (sockaddr *)&addrClient, &addrClientLen);
-	if (INVALID_SOCKET == sClient) {
-		std::cout << WSAGetLastError() << std::endl;
-		closesocket(sServer);
-		closesocket(sClient);
-		WSACleanup();
-		return false;
-	}
-
-	// 7 接收数据
-	char recvBuf[BUFFLEN];
-	ZeroMemory(recvBuf, sizeof(recvBuf));
-	if (SOCKET_ERROR == recv(sClient, recvBuf, sizeof(recvBuf), 0))
+	while (1)
 	{
-		closesocket(sServer);
+		//6 等待客户端连接
+		sockaddr_in addrClient;
+		int addrClientLen = sizeof(addrClient);
+		std::cout << "服务器启动成功,等待客户端连接中。。。" << std::endl;
+		SOCKET sClient = accept(sServer, (sockaddr *)&addrClient, &addrClientLen);
+		if (INVALID_SOCKET == sClient) {
+			std::cout << WSAGetLastError() << std::endl;
+			closesocket(sServer);
+			closesocket(sClient);
+			WSACleanup();
+			return false;
+		}
+		std::cout << "客户端(" << inet_ntoa(addrClient.sin_addr) << ":" << addrClient.sin_port << "):" << std::endl;
+		doit(sClient);
 		closesocket(sClient);
-		WSACleanup();
-		return false;
+	}
+}
+
+void doit(SOCKET fd)
+{
+	int is_static; //是否是静态请求
+	// 7 接收数据
+	char recvBuf[BUFFLEN], method[MAXBUF], uri[MAXBUF], version[MAXBUF];
+	std::string filename, cgiargs;
+
+	ZeroMemory(recvBuf, sizeof(recvBuf));
+	if (SOCKET_ERROR == recv(fd, recvBuf, sizeof(recvBuf), 0))
+	{
+		return ;
+	}
+	std::cout << "收到消息: " << recvBuf << std::endl;
+
+	sscanf(recvBuf, "%s %s %s", method, uri, version);
+
+	if (strcmp(method, "GET")) {                     //line:netp:doit:beginrequesterr
+		/*clienterror(fd, method, "501", "Not Implemented",
+			"Tiny does not implement this method");*/
+		//return;
 	}
 
-	std::cout << "客户端(" << inet_ntoa(addrClient.sin_addr) << ":" << addrClient.sin_port << "):" << recvBuf << std::endl;
+	is_static = parse_uri(uri, filename, cgiargs);
+
 	//8 发送数据
 	char sendBuf[BUFFLEN] = "你好客户端";
-	if (SOCKET_ERROR == send(sClient, sendBuf, sizeof(sendBuf), 0))
+	if (SOCKET_ERROR == send(fd, sendBuf, sizeof(sendBuf), 0))
 	{
-		closesocket(sServer);
-		closesocket(sClient);
-		WSACleanup();
-		return false;
+		return ;
 	}
-	system("pause");
-	system("pause");
-	return true;
-	std::cout << "winsocket test" << std::endl;
+}
+/*
+ * parse_uri - parse URI into filename and CGI args
+ *             return 0 if dynamic content, 1 if static
+ */
+ /* $begin parse_uri */
+//int parse_uri(char * uri, char * filename, char * cgiargs)
+int parse_uri(std::string  uri, std::string& filename, std::string& cgiargs)
+{
+	char *ptr;
+
+	if (!strstr(uri.c_str(), "cgi-bin")) {  /* Static content */ //line:netp:parseuri:isstatic
+		//strcpy(cgiargs, "");                             //line:netp:parseuri:clearcgi
+		cgiargs = "";
+		filename = ",";
+		//strcpy(filename, ".");                           //line:netp:parseuri:beginconvert1
+		filename.append(uri);
+		//strcat(filename, uri);                           //line:netp:parseuri:endconvert1
+		if (uri[uri.length() - 1] == '/')                   //line:netp:parseuri:slashcheck
+			//strcat(filename, "home.html");               //line:netp:parseuri:appenddefault
+			filename.append("home.html");
+		return 1;
+	}
+	else {  /* Dynamic content */                        //line:netp:parseuri:isdynamic
+		//ptr = index(uri, '?');                           //line:netp:parseuri:beginextract
+		size_t lo = uri.find_first_of('?');
+		if (lo != std::string::npos) {
+			cgiargs = uri.substr(lo + 1);
+		}
+		else {
+			cgiargs = "";
+		}
+	
+		/*if (ptr) {
+			strcpy(cgiargs, ptr + 1);
+			*ptr = '\0';
+		}
+		else
+			strcpy(cgiargs, "");  */                       //line:netp:parseuri:endextract
+		//strcpy(filename, ".");                           //line:netp:parseuri:beginconvert2
+		//strcat(filename, uri);                           //line:netp:parseuri:endconvert2
+		filename = ".";
+		filename.append(uri);
+		return 0;
+	}
+}
+
+void serve_dynamic(int fd, char *filename, char *cgiargs)
+{
+	char buf[BUFFLEN], *emptylist[] = { NULL };
+
+	/* Return first part of HTTP response */
+	sprintf(buf, "HTTP/1.0 200 OK\r\n");
+	//8 发送数据
+	if (SOCKET_ERROR == send(fd, buf, sizeof(buf), 0))
+		return;
+	sprintf(buf, "Server: Tiny Web Server\r\n");
+	if (SOCKET_ERROR == send(fd, buf, sizeof(buf), 0))
+		return;
+
+	
 }
